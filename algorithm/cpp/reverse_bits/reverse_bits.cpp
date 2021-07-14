284c327798f4343c1fad587cd97ab325b2723c2f
edgar.wu <edgar.wu@gmail.com>
2021-07-14 23:16:27 +0800

    [New] commit the 1st version of revers bits.

---
+++ algorithm/cpp/reverse_bits/reverse_bits.cpp
@@ -0,0 +1,80 @@
+// Source : 
+// Author : Edgar Wu
+// Date   : 14/07/2021
+// Rewrite by myself, but refer from https://github.com/haoel/leetcode
+
+/********************************************************************************** 
+* 
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+
+    Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+
+Follow up:
+
+If this function is called many times, how would you optimize it?
+
+ 
+
+Example 1:
+
+Input: n = 00000010100101000001111010011100
+Output:    964176192 (00111001011110000010100101000000)
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+Example 2:
+
+Input: n = 11111111111111111111111111111101
+Output:   3221225471 (10111111111111111111111111111111)
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+ 
+
+Constraints:
+
+    The input must be a binary string of length 32
+
+
+*               
+**********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0;
+        for (int i=0; i <32; i++) {
+            // *2 -> shift 1bit to left, +(n&0x1) => append the rightmost bit of n to it
+            // Append right most every iteration and shift to leftmost => reverst all the bits
+            ret = ret*2 + (n & 0x1);
+            // after append, shift right by 1 bit
+            n = n/2;
+        }
+        return ret;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
